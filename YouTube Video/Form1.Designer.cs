ec9c345699272098ede5392928488bce66deb5ad
philb <philb@DESKTOP-R36L41H>
2017-09-03 02:05:44 -0500

    Add project files.

---
+++ YouTube Video/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace YouTube_Video
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.tbURL = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "YouTube URL";
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(12, 39);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(888, 544);
+            this.webBrowser.TabIndex = 1;
+            this.webBrowser.Url = new System.Uri("https://www.youtube.com/v/lKfkkhEbUlY", System.UriKind.Absolute);
+            // 
+            // tbURL
+            // 
+            this.tbURL.Location = new System.Drawing.Point(95, 13);
+            this.tbURL.Name = "tbURL";
+            this.tbURL.Size = new System.Drawing.Size(711, 20);
+            this.tbURL.TabIndex = 2;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(812, 9);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(88, 27);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "&Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 595);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.tbURL);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.TextBox tbURL;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ YouTube Video/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace YouTube_Video
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string orgURL;
+        
+        public string getVideoID(string checkURL)
+        {
+            string vidID = "";
+
+            int vidIdStart = checkURL.IndexOf("/watch?v=") + 9;
+            int vidIdLength = checkURL.Length - vidIdStart;
+
+            vidID = checkURL.Substring(vidIdStart, vidIdLength);
+
+            return vidID;
+        }
+        
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            orgURL = getVideoID(tbURL.Text);
+
+            string newURL = "https://www.youtube.com/v/" + orgURL;
+
+            webBrowser.Navigate(new Uri(newURL));
+        }
+    }
+}
